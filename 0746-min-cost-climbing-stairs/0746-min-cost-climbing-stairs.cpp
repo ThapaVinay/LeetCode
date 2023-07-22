67e5116ab5609d11eacc9115854cd88e0ad19c13
Vinay Singh Thapa <97795121+ThapaVinay@users.noreply.github.com>
2023-07-22 22:36:39 +0530

    Time: 7 ms (64.99%), Space: 13.9 MB (7.70%) - LeetHub

--- 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.cpp
+++ 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.cpp
@@ -1,31 +1,53 @@
 class Solution {
 public:
     
-    int climbDp(vector <int>& cost, int index, vector <int> & dp)
+    
+    // memoization
+//     int climbDp(vector <int>& cost, int index, vector <int> & dp)
+//     {
+//         if(index >= cost.size())
+//         {
+//             return 0;
+//         }
+        
+//         if(dp[index] != -1)
+//         {
+//             return dp[index];
+//         }
+        
+//         // 1 step
+//         int take = cost[index] + climbDp(cost, index+1, dp);
+        
+//         // 2 step
+//         int not_take = cost[index] + climbDp(cost, index+2, dp);
+        
+//         dp[index] = min(take, not_take);
+//         return dp[index];
+//     }
+    
+    
+    // tabular
+    int climbDp(vector <int>& cost)
     {
-        if(index >= cost.size())
+        int n = cost.size();
+        vector <int> dp(n);
+        dp[0] = cost[0];
+        dp[1] = cost[1];
+        
+        for (int i= 2; i<n; i++)
         {
-            return 0;
+            dp[i] = min((cost[i] + dp[i-1]), (cost[i] + dp[i-2]));
         }
         
-        if(dp[index] != -1)
-        {
-            return dp[index];
-        }
+        return min(dp[n-1], dp[n-2]);
         
-        // 1 step
-        int take = cost[index] + climbDp(cost, index+1, dp);
-        
-        // 2 step
-        int not_take = cost[index] + climbDp(cost, index+2, dp);
-        
-        dp[index] = min(take, not_take);
-        return dp[index];
     }
+    
     
     int minCostClimbingStairs(vector<int>& cost) {
         int n = cost.size();
         vector <int> dp(n,-1);
-        return min(climbDp(cost, 0,dp), climbDp(cost,1, dp));
+        // return min(climbDp(cost, 0,dp), climbDp(cost,1, dp));
+        return climbDp(cost);
     }
 };
