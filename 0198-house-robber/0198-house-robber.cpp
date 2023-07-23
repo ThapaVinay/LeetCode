08cff04adefea8e3aa13bffa00b95e3b6e654a97
Vinay Singh Thapa <97795121+ThapaVinay@users.noreply.github.com>
2023-07-23 11:41:48 +0530

    Time: 0 ms (100.00%), Space: 7.7 MB (74.80%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     
+    // memoization
     int robDP(vector <int> &nums, int index, vector <int> &dp)
     {
         if(index >= nums.size())
@@ -23,9 +24,32 @@ public:
     }
     
     
+    // tabular
+    int robTabular(vector <int> &nums)
+    {
+        vector <int>dp(nums.size()+1,0);
+        
+        if(nums.size() == 1)
+        {
+            return nums[0];
+        }
+        
+        dp[0] = nums[0];
+        dp[1] = max(nums[1], nums[0]);
+        for(int i=2; i<nums.size(); i++)
+        {
+            dp[i] = max((nums[i] + dp[i-2]), dp[i-1]);
+        }
+        
+        return dp[nums.size()-1];
+    }
+    
+    
     int rob(vector<int>& nums) {
         
-        vector <int> dp (nums.size(), -1);
-        return robDP(nums, 0, dp);
+        // vector <int> dp (nums.size(), -1);
+        // return robDP(nums, 0, dp);
+        
+        return robTabular(nums);
     }
 };
