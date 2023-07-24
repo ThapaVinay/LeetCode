63d729460cea355d922859daa50220ce1773d617
Vinay Singh Thapa <97795121+ThapaVinay@users.noreply.github.com>
2023-07-24 12:52:42 +0530

    Time: 5 ms (38.63%), Space: 6.6 MB (79.44%) - LeetHub

--- 0377-combination-sum-iv/0377-combination-sum-iv.cpp
+++ 0377-combination-sum-iv/0377-combination-sum-iv.cpp
@@ -1,6 +1,27 @@
 class Solution {
 public:
     
+    // tabular
+    int solveTab(vector <int> &nums, int t)
+    {
+        vector <uint> dp(t+1);
+        
+        dp[0] = 1;
+        
+        for(int i=1; i<=t; i++)
+        {
+            for(int j=0; j<nums.size(); j++)
+            {
+                if(i-nums[j] >= 0)
+                    dp[i] += dp[i-nums[j]];
+            }
+        }
+        return dp[t];
+    }
+    
+    
+    
+    // memoization
     int solveDP(vector<int> &nums, int t, vector <int> &dp)
     {
         if(t == 0)
@@ -24,14 +45,15 @@ public:
             ans += solveDP(nums, t- nums[i], dp);
         }
         
-        
         return dp[t] = ans;
         
     }
     
     int combinationSum4(vector<int>& nums, int target) {
         
-        vector <int> dp (target+1, -1);
-        return solveDP(nums, target, dp);
+        // vector <int> dp (target+1, -1);
+        // return solveDP(nums, target, dp);
+        
+        return solveTab(nums, target);
     }
 };
