6e75e9baeda4f11fe5bb74d5b2eab3f18f678588
Vinay Singh Thapa <97795121+ThapaVinay@users.noreply.github.com>
2023-07-25 21:17:42 +0530

    Time: 126 ms (60.03%), Space: 59.6 MB (40.39%) - LeetHub

--- 0852-peak-index-in-a-mountain-array/0852-peak-index-in-a-mountain-array.cpp
+++ 0852-peak-index-in-a-mountain-array/0852-peak-index-in-a-mountain-array.cpp
@@ -5,7 +5,7 @@ public:
         int start = 0;
         int end = arr.size()-1;
         int mid = (start + end)/2;
-        while(mid >= 0)
+        while(start<end)
         {
             if(arr[mid] > arr[mid-1] && arr[mid] > arr[mid+1])
             {
