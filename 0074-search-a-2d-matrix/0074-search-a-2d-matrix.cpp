660173f18e4639c0fa16f73e44bdfef106ad2928
Vinay Singh Thapa <97795121+ThapaVinay@users.noreply.github.com>
2023-08-13 21:50:04 +0530

    Time: 9 ms (7.66%), Space: 9.5 MB (66.07%) - LeetHub

---
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        // use binary search
+        // count total elements and find the mid value
+        // how to assign x & y using mid value ?
+        // 6/4 = 1 , 6 % 4 = 2
+        
+        int m = matrix.size();
+        int n = matrix[0].size();
+        int start = 0;
+        int end = (m * n) - 1;
+        
+        while(start <= end)
+        {
+            int mid = start + (end-start)/2;
+            int x = mid/n;
+            int y = mid%n;
+            
+            if(matrix[x][y] == target)
+            {
+                return true;
+            }
+            
+            if(matrix[x][y] > target)
+            {
+                end = mid-1;
+            }
+            
+            if(matrix[x][y] < target)
+            {
+                start = mid+1;
+            }
+            
+            cout << start << " " << end << endl;
+        }
+        
+        return false;
+        
+    }
+};
