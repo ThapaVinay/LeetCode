19e8d055deb050d76139a57c74b05e21f60f65e9
Vinay Singh Thapa <97795121+ThapaVinay@users.noreply.github.com>
2023-08-26 13:21:59 +0530

    Time: 108 ms (69.04%), Space: 22.6 MB (73.82%) - LeetHub

--- 0646-maximum-length-of-pair-chain/0646-maximum-length-of-pair-chain.cpp
+++ 0646-maximum-length-of-pair-chain/0646-maximum-length-of-pair-chain.cpp
@@ -21,27 +21,12 @@ public:
                     min = pairs[j][1];
                     index = j;
                 }
-                // else if(min == pairs[j][1])
-                // {
-                //     if(pairs[index][1] > pairs[j][1])
-                //     {
-                //         min = pairs[j][0];
-                //         index = j;
-                //     }
-                // }
             }
             
             swap(pairs[i][0], pairs[index][0]);
             swap(pairs[i][1], pairs[index][1]); 
         }
-        
-        for(int i=0; i<pairs.size(); i++)
-        {
-            cout << pairs[i][0] << " " << pairs[i][1] << endl;
-        }
-        cout << " ########### " << endl; 
-        
-        
+    
         int count = 1;
         int j = 0;
         for(int i=1; i<pairs.size(); i++)
