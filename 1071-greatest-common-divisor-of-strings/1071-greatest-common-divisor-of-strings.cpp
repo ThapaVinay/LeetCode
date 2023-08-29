0309ad0ff3706e2ea2790ea06877af35f6bc67ef
Vinay Singh Thapa <97795121+ThapaVinay@users.noreply.github.com>
2023-08-29 22:45:06 +0530

    Time: 0 ms (100.00%), Space: 7.2 MB (33.33%) - LeetHub

--- 1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.cpp
+++ 1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.cpp
@@ -1,59 +1,10 @@
 class Solution {
 public:
     string gcdOfStrings(string str1, string str2) {
-        
-        
-        string res = "";
-        string temp = "";
-        int i =0;
-        for(i=0; i<str1.size() && i<str2.size(); i++)
-        {
-            if(str1[i] == str2[i])
-            {
-                temp += str1[i];
-            }
-            else{
-                return "";
-            }
-            
-            if(temp.size() > 0 && str1.size() % temp.size() == 0 && str2.size() % temp.size() == 0)
-            {
-                res = temp;
-            }
-            
-        }
-        
-        if(str1.size() == 3 && str2.size() == 2 && res == "A")
-        {
+        // Check if concatenated strings are equal or not, if not return ""
+        if(str1 + str2 != str2 + str1)
             return "";
-        }
-        
-        int j=0;
-        if(i == str1.size() )
-        {
-            while(i<str2.size())
-            {
-                if(res[j] != str2[i])
-                {
-                    return "";
-                }
-                j= (j+1)%res.size();
-                i++;
-            }
-        }
-        else if(i == str2.size())
-        {
-            while(i<str1.size())
-            {
-                if(res[j] != str1[i])
-                {
-                    return "";
-                }
-                j= (j+1)%res.size();
-                i++;
-            }
-        }
-        
-        return res;
+        // If strings are equal than return the substring from 0 to gcd of size(str1), size(str2)
+        return str1.substr(0, gcd(str1.size(), str2.size()));
     }
 };
