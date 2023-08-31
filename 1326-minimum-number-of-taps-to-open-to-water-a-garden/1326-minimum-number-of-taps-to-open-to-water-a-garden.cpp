56488010508e5bf3c50a4f6fd777ff0eafeb6b41
Vinay Singh Thapa <97795121+ThapaVinay@users.noreply.github.com>
2023-08-31 17:56:28 +0530

    Time: 46 ms (40.13%), Space: 14.1 MB (80.27%) - LeetHub

---
+++ 1326-minimum-number-of-taps-to-open-to-water-a-garden/1326-minimum-number-of-taps-to-open-to-water-a-garden.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int minTaps(int n, vector<int>& ranges) {
+        
+        int min = 0;
+        int max = 0;
+        int total = 0;
+        
+        while(max < n)
+        {
+            for(int i=0; i<ranges.size(); i++)
+            {
+                if((i - ranges[i]) <= min && (i + ranges[i] > max))
+                {
+                    max = i + ranges[i];
+                }
+            }
+            
+            if(min == max)
+            {
+                return -1;
+            }
+            
+            min = max;
+            total ++;
+        }
+        
+        return total;
+    }
+};
