38291595e06c2c87a9d102a6a67075cf8e0a66e9
Vinay Singh Thapa <97795121+ThapaVinay@users.noreply.github.com>
2023-09-01 10:28:27 +0530

    Time: 7 ms (40.85%), Space: 8.3 MB (42.13%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.cpp
+++ 0338-counting-bits/0338-counting-bits.cpp
@@ -1,33 +1,38 @@
 class Solution {
 public:
     
-    int countDP(int n)
+    int countDP(int n, vector <int> &dp)
     {
         if(n == 0)
         {
             return 0;
         }
         
+        if(dp[n] != -1)
+            return dp[n];
+        
         int a;
         if(n % 2 == 1)
-            a = 1 + countDP(n/2);
+            a = 1 + countDP(n/2, dp);
         
         else if(n % 2 == 0)
-            a = countDP(n/2);
+            a = countDP(n/2, dp);
         
-        return a;
-            
-        
+        dp[n] = a;
+        return dp[n];
+ 
     }
     
     
     vector<int> countBits(int n) {
         
         vector <int> res(n+1);
+        vector <int> dp(n+1, -1);
+        
         res[0] = 0;
         for(int i=1; i<=n; i++)
         {
-            res[i] = countDP(i);
+            res[i] = countDP(i, dp);
         }
         return res;
     }
