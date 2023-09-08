e5fa227fb45085431b9f8faa1af40042e1fe8836
Vinay Singh Thapa <97795121+ThapaVinay@users.noreply.github.com>
2023-09-08 10:49:59 +0530

    Time: 108 ms (17.52%), Space: 30.7 MB (8.34%) - LeetHub

---
+++ 0264-ugly-number-ii/0264-ugly-number-ii.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int nthUglyNumber(int n) {
+        if (n <= 0) return 0;
+
+        priority_queue<long long, vector<long long>, greater<long long>> pq;
+        unordered_set<long long> seen;
+        vector<int> primes = {2, 3, 5};
+        
+        pq.push(1);
+        seen.insert(1);
+
+        long long ugly = 1;
+        for (int i = 0; i < n; i++) {
+            ugly = pq.top();
+            pq.pop();
+
+            for (int prime : primes) {
+                long long next = ugly * prime;
+                if (!seen.count(next)) {
+                    pq.push(next);
+                    seen.insert(next);
+                }
+            }
+        }
+
+        return ugly;
+    }
+};
