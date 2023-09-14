9af9d7971622a98f7361729990ac1c776bcaedc4
Vinay Singh Thapa <97795121+ThapaVinay@users.noreply.github.com>
2023-09-14 17:01:46 +0530

    Time: 29 ms (26.41%), Space: 15.3 MB (36.34%) - LeetHub

---
+++ 0332-reconstruct-itinerary/0332-reconstruct-itinerary.cpp
@@ -0,0 +1,46 @@
+class Solution {
+public:
+    bool found = false;
+    void dfs(map<string, vector<string>> &mp, string check,int s, vector <string> &res)
+    {
+        if(res.size() == s)
+        {
+            found = true;
+            return;
+        }
+        
+        if(mp[check].size() == 0)
+        {
+            return;
+        }
+        
+        for(int x = mp[check].size()-1; x >=0 ;x--)
+        {
+            res.push_back(mp[check][x]);
+            string temp = mp[check][x];
+            mp[check].erase(mp[check].begin() + x);
+            dfs(mp, temp, s, res);
+            if(found)
+                break;
+            
+            mp[check].insert(mp[check].begin() + x, temp);
+            res.pop_back();
+        }
+    }
+    
+    vector<string> findItinerary(vector<vector<string>>& tickets) {
+        
+        map <string, vector<string>> mp;
+        vector <string> res;
+        
+        for(auto vec : tickets)
+        {
+            mp[vec[0]].push_back(vec[1]);
+            sort(mp[vec[0]].begin(), mp[vec[0]].end(), greater<string>());
+        }
+
+        res.push_back("JFK");
+        dfs(mp, "JFK", tickets.size() + 1, res);
+        return res;
+    }
+};
