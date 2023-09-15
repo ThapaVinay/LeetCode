8d4e064422e7a1cc594091c29be38f60de8f210f
Vinay Singh Thapa <97795121+ThapaVinay@users.noreply.github.com>
2023-09-15 18:27:37 +0530

    Time: 118 ms (92.42%), Space: 57.7 MB (18.14%) - LeetHub

--- 0933-number-of-recent-calls/0933-number-of-recent-calls.cpp
+++ 0933-number-of-recent-calls/0933-number-of-recent-calls.cpp
@@ -1,24 +1,13 @@
 class RecentCounter {
 public:
-    vector <int> res;
+    queue<int> q;
+
     RecentCounter() {
     }
-    
-    int ping(int t) { 
-        res.push_back(t);
-        int s = t - 3000;
-        int count = 0;
-        for(int i=0; i<res.size(); i++)
-        {
-            if(res[i] >= s && res[i] <= t)
-                count ++;
-        }
-        return count;
+
+    int ping(int t) {
+        q.push(t);
+        while (q.front() < t - 3000) q.pop();
+        return q.size();
     }
 };
-
-/**
- * Your RecentCounter object will be instantiated and called as such:
- * RecentCounter* obj = new RecentCounter();
- * int param_1 = obj->ping(t);
- */
