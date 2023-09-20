542ee212e90d9c6a6bf71183b1380fc8afa45ad6
Vinay Singh Thapa <97795121+ThapaVinay@users.noreply.github.com>
2023-09-20 10:15:03 +0530

    Time: 136 ms (44.79%), Space: 98.6 MB (44.22%) - LeetHub

--- 1658-minimum-operations-to-reduce-x-to-zero/1658-minimum-operations-to-reduce-x-to-zero.cpp
+++ 1658-minimum-operations-to-reduce-x-to-zero/1658-minimum-operations-to-reduce-x-to-zero.cpp
@@ -2,25 +2,24 @@ class Solution {
 public:
     
     int minOperations(vector<int>& nums, int x) {
-    
-          static int N = [] () {
-            ios::sync_with_stdio(false); 
-            cin.tie(0); 
-            return 0;
-        } ();
+        
         int s = nums.size();
-        int Sum = 0;
+        int sum = 0;
         int res = INT_MAX;
+        
         int target = accumulate(nums.begin(), nums.end(), 0) - x;
-        for (int l = 0, r = 0, Sum = 0; r < s; r++) {
-            Sum += nums[r];
-            while ( Sum > target && l <= r) {
-                Sum -= nums[l];
+        
+        for (int l = 0, r = 0, sum = 0; r < s; r++) 
+        {
+            sum += nums[r];
+            while ( sum > target && l <= r) {
+                sum -= nums[l];
                 l++;
             }
-            if (Sum == target)
-                res = std::min(res, s-(r-l+1));
+            if (sum == target)
+                res = min(res, s-(r-l+1));
         }
+        
         return res == INT_MAX ? -1 : res;
         
     }
