2a4017d965d9fdf03f57ee8ccbdc4aa1e72b29fc
Vinay Singh Thapa <97795121+ThapaVinay@users.noreply.github.com>
2023-09-26 16:36:11 +0530

    Time: 3 ms (48.89%), Space: 6.7 MB (15.47%) - LeetHub

---
+++ 1081-smallest-subsequence-of-distinct-characters/1081-smallest-subsequence-of-distinct-characters.cpp
@@ -0,0 +1,49 @@
+class Solution {
+public:
+    string smallestSubsequence(string s) {
+        stack <char> st;
+        vector <int> count(26, 0);
+        vector <int> visited(26,0);
+        
+        // saving the last occurance
+        for(int i=0; i<s.size(); i++)
+        {
+            count[s[i] - 'a'] = i;
+        }
+        
+        st.push(s[0]);
+        visited[s[0] - 'a'] = 1;
+        for(int i=1; i<s.size(); i++)
+        {
+            char ch = s[i];
+            if(visited[ch-'a'] == 0)
+            {
+                if(st.top() < ch)
+                {
+                    st.push(ch);
+                    visited[ch - 'a'] = 1;
+                }
+                else {
+                    while(!st.empty() && st.top() > ch && count[st.top() - 'a'] > i)
+                    {
+                        visited[st.top() - 'a'] = 0;
+                        st.pop();
+                    }
+                    st.push(ch);
+                    visited[ch - 'a'] = 1;
+                }
+
+            }
+            
+        }
+        
+        string res = "";
+        while(!st.empty())
+        {
+            res = st.top() + res;
+            st.pop();
+        }
+        
+        return res;
+    }
+};
