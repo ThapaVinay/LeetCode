d23cf08bc27903d010240424138fb8a9ec70af29
Vinay Singh Thapa <97795121+ThapaVinay@users.noreply.github.com>
2023-09-28 13:27:35 +0530

    Time: 23 ms (54.96%), Space: 25.5 MB (36.67%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.cpp
+++ 0189-rotate-array/0189-rotate-array.cpp
@@ -5,9 +5,9 @@ public:
         int n = nums.size();
         k = k % n;
         
-        reverse(nums.begin(), nums.end()-k);
-        reverse(nums.begin() + (n-k), nums.end());
-        reverse(nums.begin(), nums.end());
+        reverse(nums.begin(), nums.end()-k); // 0 -> n-k
+        reverse(nums.begin() + (n-k), nums.end());  // n-k -> n
+        reverse(nums.begin(), nums.end()); // 0 -> n
         
     }
 };
