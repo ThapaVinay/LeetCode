b2069e705ee350ff1905b477bcea960eed5f6452
Vinay Singh Thapa <97795121+ThapaVinay@users.noreply.github.com>
2023-09-28 22:18:46 +0530

    Time: 2 ms (55.54%), Space: 6.3 MB (19.69%) - LeetHub

--- 0556-next-greater-element-iii/0556-next-greater-element-iii.cpp
+++ 0556-next-greater-element-iii/0556-next-greater-element-iii.cpp
@@ -4,29 +4,20 @@ public:
         // check from last if we have a integer greater than that in the right side
         // if there is then just swap and sort the array after that index
         
+        string s = to_string(n);
         
-        vector <int> nums;
-        
-        while(n != 0)
-        {
-            int rem = n%10;
-            nums.push_back(rem);
-            n = n/10;
-        }
-        
-        int size = nums.size();
-        reverse(begin(nums), end(nums));
+        int size = s.size();
         
         // find the greater element 
         int i, j=size-1;
         for(i=size-2; i>=0; i--)
         {
-            if(nums[i] >= nums[j])
+            if(s[i] >= s[j])
             {
                 j--;
             }
             else{
-                while(j < size && nums[i] < nums[j])
+                while(j < size && s[i] < s[j])
                 {
                     j++;
                 }
@@ -40,20 +31,12 @@ public:
             return -1;
         
         // swap them
-        swap(nums[i], nums[j]);
+        swap(s[i], s[j]);
         
         // sort the array from i+1
-        sort(nums.begin() + i+1, nums.end());
+        sort(s.begin() + i+1, s.end());
         
-        long res = 0;
-        for(i=0; i<size; i++)
-        {
-            res += nums[i];
-            
-            if(i < size-1)
-                res *= 10;
-            
-        }
+        long res = stoll(s);
         
         if(res > INT_MAX)
             return -1;
