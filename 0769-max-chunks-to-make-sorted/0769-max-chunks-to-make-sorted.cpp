8ce3a09c19acbb10d05cd595a6e3a986e4263895
Vinay Singh Thapa <97795121+ThapaVinay@users.noreply.github.com>
2023-09-28 23:12:52 +0530

    Time: 3 ms (40.44%), Space: 7.5 MB (14.93%) - LeetHub

---
+++ 0769-max-chunks-to-make-sorted/0769-max-chunks-to-make-sorted.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int maxChunksToSorted(vector<int>& arr) {
+        
+        int maxi = 0;
+        int count = 0;
+        
+        for(int i=0; i<arr.size(); i++)
+        {
+            maxi = max(maxi, arr[i]);
+            if(maxi == i)
+            {
+                count ++;
+            }
+        }
+        
+        return count;
+    }
+};
