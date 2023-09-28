ce4743d088ec3ebe58f6edbd7e679a15b0c0dbec
Vinay Singh Thapa <97795121+ThapaVinay@users.noreply.github.com>
2023-09-28 14:18:19 +0530

    Time: 74 ms (89.34%), Space: 68.3 MB (5.74%) - LeetHub

---
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        
+        int maxi = nums[0];
+        int local = 0;
+        
+        for(int i=0; i<nums.size(); i++)
+        {
+            local += nums[i];
+            
+            maxi = max(local, maxi);
+            
+            if(local <= 0){
+                local = 0;
+            }
+        }
+        
+        return maxi;
+    }
+};
