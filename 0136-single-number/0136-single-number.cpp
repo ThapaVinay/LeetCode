a65bba575d74ef84daea92cda541fa170a35d529
Vinay Singh Thapa <97795121+ThapaVinay@users.noreply.github.com>
2023-10-02 14:04:34 +0530

    Time: 7 ms (97.95%), Space: 17.3 MB (24.61%) - LeetHub

--- 0136-single-number/0136-single-number.cpp
+++ 0136-single-number/0136-single-number.cpp
@@ -5,7 +5,7 @@ public:
         int res = 0;
         for(int i=0; i<nums.size(); i++)
         {
-            res = res ^ nums[i];
+            res = res^nums[i];
         }
         return res;
     }
