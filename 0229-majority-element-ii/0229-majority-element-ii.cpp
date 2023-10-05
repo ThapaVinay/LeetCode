4f27b9ed1cc96db470cc6fb8c1f1d7fd0a15fb9e
Vinay Singh Thapa <97795121+ThapaVinay@users.noreply.github.com>
2023-10-05 11:38:42 +0530

    Time: 11 ms (51.76%), Space: 16.4 MB (9.38%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.cpp
+++ 0229-majority-element-ii/0229-majority-element-ii.cpp
@@ -1,26 +1,60 @@
 class Solution {
 public:
-  vector<int> majorityElement(vector<int> &a) 
-  {
-	  int y(-1), z(-1), cy(0), cz(0);
-    
-	  for (const auto & x: a) 
-	  {
-		  if (x == y) cy++;
-		  else if (x == z) cz++;
-		  else if (! cy) y = x, cy = 1;
-		  else if (! cz) z = x, cz = 1;
-		  else cy--, cz--;
-     }
-      
-     cy = cz = 0;
-     for (const auto & x: a)
-		 if (x == y) cy++;
-			 else if (x == z) cz++;
-  
-	  vector<int> r;
-	  if (cy > size(a)/3) r.push_back(y);
-	  if (cz > size(a)/3) r.push_back(z);
-	  return r;
-  }
+    vector<int> majorityElement(vector<int>& nums) {
+        
+        if(nums.size() == 1)
+        {
+            return {nums[0]};
+        }
+        
+        int a = nums[0], b = nums[1];
+        int count1 = 0, count2 = 0;
+        
+        for(int i=0; i<nums.size(); i++)
+        {
+            if(a == nums[i])
+                count1 ++;
+            else if(b == nums[i])
+                count2 ++;
+            else if(count1 == 0)
+            {
+                count1 = 1;
+                a = nums[i];
+            }
+            else if(count2 == 0)
+            {
+                count2 = 1;
+                b = nums[i];
+            }
+            else{
+                count1 --;
+                count2 --;
+            }
+        }
+        
+        count1 = count2 = 0;
+        for (int i = 0; i<nums.size(); i++)
+        {
+            if (nums[i] == a)
+                count1++;
+            else if (nums[i] == b)
+                count2++;
+        }
+        
+        vector <int> res;
+                
+        if(count1 > floor(nums.size()/3))
+        {
+            res.push_back(a);
+        }
+        
+        if(count2 > floor(nums.size()/3))
+        {
+            res.push_back(b);
+        }
+        
+        
+                
+        return res;
+    }
 };
