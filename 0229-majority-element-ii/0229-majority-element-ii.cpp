271a71237251dbd85e101c715b3dca6ffd2a4991
Vinay Singh Thapa <97795121+ThapaVinay@users.noreply.github.com>
2023-10-05 11:33:58 +0530

    Time: 11 ms (51.76%), Space: 16.4 MB (9.38%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.cpp
+++ 0229-majority-element-ii/0229-majority-element-ii.cpp
@@ -1,32 +1,26 @@
 class Solution {
 public:
-    vector<int> majorityElement(vector<int>& nums) {
-        
-        
-        sort(begin(nums), end(nums));
-        vector <int> res;
-        
-        int count = 0;
-        int prev = nums[0];
-        int majority = nums.size()/3;
-        
-        for(int i=0; i<nums.size(); i++)
-        {
-            if(prev == nums[i])
-            {
-                count ++;
-            }
-            else{
-                prev = nums[i];
-                count = 1;
-            }
-            
-            if(count == majority+1)
-            {
-                res.push_back(nums[i]);
-            }
-        }
-        
-        return res;
-    }
+  vector<int> majorityElement(vector<int> &a) 
+  {
+	  int y(-1), z(-1), cy(0), cz(0);
+    
+	  for (const auto & x: a) 
+	  {
+		  if (x == y) cy++;
+		  else if (x == z) cz++;
+		  else if (! cy) y = x, cy = 1;
+		  else if (! cz) z = x, cz = 1;
+		  else cy--, cz--;
+     }
+      
+     cy = cz = 0;
+     for (const auto & x: a)
+		 if (x == y) cy++;
+			 else if (x == z) cz++;
+  
+	  vector<int> r;
+	  if (cy > size(a)/3) r.push_back(y);
+	  if (cz > size(a)/3) r.push_back(z);
+	  return r;
+  }
 };
