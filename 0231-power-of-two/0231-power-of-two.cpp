d18c90de5449d71f8937187cca878c4d8b4416e3
Vinay Singh Thapa <97795121+ThapaVinay@users.noreply.github.com>
2024-01-12 12:05:15 +0530

    Time: 0 ms (100.00%), Space: 6.2 MB (81.53%) - LeetHub

---
+++ 0231-power-of-two/0231-power-of-two.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool isPowerOfTwo(int n) {
+        
+        while(n != 0)
+        {
+            if(n == 1)
+            {
+                return true;
+            }
+            
+            if(n % 2 != 0)
+                return false;
+            
+            n = n >> 1;
+        }
+        
+        return false;
+    }
+};
