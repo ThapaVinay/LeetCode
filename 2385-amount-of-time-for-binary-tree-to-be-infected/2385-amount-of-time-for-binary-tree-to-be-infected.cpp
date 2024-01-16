4a5dc50e2fd5d2a3ffeda5b87e1657a1296f6f1e
Vinay Singh Thapa <97795121+ThapaVinay@users.noreply.github.com>
2024-01-16 23:10:40 +0530

    Time: 756 ms (13.50%), Space: 411.9 MB (9.25%) - LeetHub

---
+++ 2385-amount-of-time-for-binary-tree-to-be-infected/2385-amount-of-time-for-binary-tree-to-be-infected.cpp
@@ -0,0 +1,79 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    unordered_map <int, unordered_set<int>> mp;
+    
+    void traverse(TreeNode *root, int parent)
+    {
+        if(root == nullptr)
+        {
+            return;
+        }
+        
+        if(parent != 0)
+        {
+            mp[parent].insert(root->val);
+            mp[root -> val].insert(parent);
+        }
+        
+        traverse(root->left, root->val);
+        traverse(root->right, root->val);
+        
+    }
+    
+    int amountOfTime(TreeNode* root, int start) {
+        
+        traverse(root, 0);
+        
+        // for(auto pair: mp)
+        // {
+        //     cout << pair.first << " => ";
+        //     for(auto i: pair.second)
+        //     {
+        //         cout << i << " , ";
+        //     }
+        //     cout << endl;
+        // }
+        
+        queue <int> q;
+        q.push(start);
+        
+        unordered_set <int> visited;
+        visited.insert(start);
+        
+        int count = -1;
+        // cout << "#" << endl;
+        while(!q.empty())
+        {
+            int size = q.size();
+            // cout << size << endl;
+            while(size > 0)
+            {
+                int front = q.front();
+                q.pop();
+                
+                for(auto x: mp[front])
+                {
+                    if(!visited.contains(x))
+                    {
+                        q.push(x);
+                        visited.insert(x);
+                    }     
+                }
+                size --;
+            }
+            count ++;
+        }
+        return count;
+    }
+};
