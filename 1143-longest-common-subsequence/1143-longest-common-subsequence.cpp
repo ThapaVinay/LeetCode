5ad8c32c41f766694adc4f86e8383dcc3645d1d4
Vinay Singh Thapa <97795121+ThapaVinay@users.noreply.github.com>
2024-01-20 21:48:47 +0530

    Time: 15 ms (97.20%), Space: 25.7 MB (6.27%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
@@ -25,13 +25,36 @@ public:
         return dp[i][j] = max(x, y);
     }
     
+    int recurTab(string &text1, string &text2)
+    {
+        vector <vector<int>> dp(text1.size()+1, vector<int>(text2.size()+1, 0));
+        
+        for(int i=text1.size()-1; i>=0; i--)
+        {
+            for(int j=text2.size()-1; j>=0; j--)
+            {
+                if(text1[i] == text2[j])
+                {
+                    dp[i][j] = 1 + dp[i+1][j+1];
+                }
+                else{
+                    dp[i][j] = max(dp[i+1][j], dp[i][j+1]);
+                }
+            }
+        }
+        
+        return dp[0][0];
+    }
+    
     int longestCommonSubsequence(string text1, string text2) {
         
         // if both characters are same then increment i & j
         // if not equal then either increment i or j
         
-        vector <vector<int>> dp(text1.size(), vector<int>(text2.size(), -1));
+        // vector <vector<int>> dp(text1.size(), vector<int>(text2.size(), -1));
         
-        return recurDP(text1, text2, 0 , 0, dp);
+        // return recurDP(text1, text2, 0 , 0, dp);
+        
+        return recurTab(text1, text2);
     }
 };
