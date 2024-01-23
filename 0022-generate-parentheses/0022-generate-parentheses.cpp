3568d09ed636bff626002c3d106e7e158139b9aa
Vinay Singh Thapa <97795121+ThapaVinay@users.noreply.github.com>
2024-01-23 15:16:21 +0530

    Time: 51 ms (5.01%), Space: 86.1 MB (5.01%) - LeetHub

---
+++ 0022-generate-parentheses/0022-generate-parentheses.cpp
@@ -0,0 +1,66 @@
+class Solution {
+public:
+    
+    unordered_set <string> res;
+    
+    bool check(string &s)
+    {
+        stack <char> st;
+        
+        for(auto &ch: s)
+        {
+            if(st.empty())
+            {
+                st.push(ch);
+            }
+            else if(st.top() == '(' && ch == ')')
+            {
+                st.pop();
+            }
+            else{
+                st.push(ch);
+            }
+        }
+        
+        return st.size() == 0;
+    }
+    
+    void recur(int n, string &s)
+    {
+        if(s.size() >= n)
+        {
+            if(check(s))
+            {
+                res.insert(s);
+                return;
+            }
+            return;
+        }
+        
+        // adding '('
+        s.push_back('(');
+        recur(n, s);
+        s.pop_back();
+        
+        if(s.size() != 0) 
+        {
+            s.push_back(')');
+            recur(n, s);
+            s.pop_back();
+        }
+        
+    }
+    
+    vector<string> generateParenthesis(int n) {
+        
+        string temp = "";
+        recur(2*n, temp);
+        
+        vector <string> ans;
+        for(auto &x: res)
+            ans.push_back(x);
+        
+        return ans;
+        
+    }
+};
