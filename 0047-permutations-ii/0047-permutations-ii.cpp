a2d533e0930a51f5c595f204a5a61bfb2067339d
Vinay Singh Thapa <97795121+ThapaVinay@users.noreply.github.com>
2024-01-25 13:49:01 +0530

    Time: 76 ms (27.19%), Space: 12.1 MB (27.73%) - LeetHub

---
+++ 0047-permutations-ii/0047-permutations-ii.cpp
@@ -0,0 +1,52 @@
+class Solution {
+public:
+    
+    set <vector<int>> res;
+    
+    void recur(vector <int> &nums, vector <int> &temp, vector <int> &index)
+    {
+        if(temp.size() == nums.size())
+        {
+            if(res.count(temp))
+                return;
+            
+            res.insert(temp);
+            
+            return;
+        }
+        
+        
+        for(int i=0; i<nums.size(); i++)
+        {
+            if(find(index.begin(), index.end(), i) != index.end())
+                continue;
+            
+            index.push_back(i);
+            temp.push_back(nums[i]);
+            
+            recur(nums, temp, index);
+            
+            temp.pop_back();
+            index.pop_back();
+        }
+    }
+    
+    vector<vector<int>> permuteUnique(vector<int>& nums) {
+        
+        vector <int> temp;
+        vector <int> index;
+        
+        sort(nums.begin(), nums.end());
+        
+        recur(nums, temp, index);
+        
+        vector <vector<int>> ans;
+        
+        for(auto x: res)
+        {
+            ans.push_back(x);
+        }
+        
+        return ans;
+    }
+};
