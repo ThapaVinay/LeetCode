8810e03267a0553c5e56c519f1b56186c7483eb3
Vinay Singh Thapa <97795121+ThapaVinay@users.noreply.github.com>
2024-01-25 18:02:09 +0530

    Time: 1493 ms (5.00%), Space: 51.2 MB (10.90%) - LeetHub

--- 0055-jump-game/0055-jump-game.cpp
+++ 0055-jump-game/0055-jump-game.cpp
@@ -24,10 +24,31 @@ public:
         return dp[i] = flag;
     }
     
+    bool recurTab(vector <int> &nums)
+    {
+        vector <bool> dp(nums.size()+1, false);
+        
+        dp[nums.size()-1] = true;
+        
+        for(int i=nums.size()-2; i>=0; i--)
+        {
+            bool flag = false;
+            for(int j=1; j<=nums[i] && i+j < nums.size(); j++)
+            {
+                flag = flag || dp[i+j];
+            }
+            dp[i] = flag;
+        }
+        
+        return dp[0]; 
+    }
+    
     bool canJump(vector<int>& nums) {
         
-        vector <int> dp(nums.size(), -1);
-        return recurDP(nums, 0, dp);
+        // vector <int> dp(nums.size(), -1);
+        // return recurDP(nums, 0, dp);
+        
+        return recurTab(nums);
         
     }
 };
