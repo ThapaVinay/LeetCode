557b2b0f8a3f8b4ea7426e2692228503c289e280
Vinay Singh Thapa <97795121+ThapaVinay@users.noreply.github.com>
2024-01-27 13:59:05 +0530

    Time: 1825 ms (9.34%), Space: 17.6 MB (10.99%) - LeetHub

--- 0629-k-inverse-pairs-array/0629-k-inverse-pairs-array.cpp
+++ 0629-k-inverse-pairs-array/0629-k-inverse-pairs-array.cpp
@@ -26,11 +26,36 @@ public:
         return dp[n][k] = (res %MOD);
     }
     
+    int recurTab(int n, int k)
+    {
+        vector <vector<int>> dp(n+1, vector<int>(k+1, 0));
+        
+        for(int i=0; i<=n; i++)
+        {
+            dp[i][0] = 1;
+        }
+        
+        for(int i=1; i<=n; i++)
+        {
+            for(int j=1; j<=k; j++)
+            {
+                for(int inv=0; inv <= min(i-1, j); inv++)
+                {
+                    dp[i][j] = (dp[i][j] + dp[i-1][j-inv]) % MOD;
+                }
+            }
+        }
+        
+        return dp[n][k];
+    }
+    
     int kInversePairs(int n, int k) {
         
         // just iterate through k and for each element you have the option that it will make 1, 2, 3 ... k inverse pairs
         
-        vector <vector<int>> dp(n+1, vector<int>(k+1, -1));
-        return recurDP(n, k, dp);
+        // vector <vector<int>> dp(n+1, vector<int>(k+1, -1));
+        // return recurDP(n, k, dp);
+        
+        return recurTab(n, k);
     }
 };
