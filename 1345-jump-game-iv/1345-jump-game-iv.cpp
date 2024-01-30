25879624c59cdd547d082db95e0d2fa844210552
Vinay Singh Thapa <97795121+ThapaVinay@users.noreply.github.com>
2024-01-30 22:37:12 +0530

    Time: 175 ms (62.41%), Space: 100.3 MB (27.42%) - LeetHub

---
+++ 1345-jump-game-iv/1345-jump-game-iv.cpp
@@ -0,0 +1,68 @@
+class Solution {
+public:
+    
+    int minJumps(vector<int>& arr) {
+        // first save the elements with their occurances in a map to make graph
+        // then use bfs to iterate using queue
+        
+        unordered_map <int, vector<int>> mp;
+        
+        int n = arr.size();
+        
+        for(int i=0; i<n; i++)
+        {
+            mp[arr[i]].push_back(i);
+        }
+        
+        // use bfs
+        queue <int> q;
+        q.push(0);
+        int steps = 0;
+        
+        vector <int> visited(n, 0);
+        visited[0] = 1;
+        
+        while(!q.empty())
+        {
+            int k = q.size();
+            while(k--)
+            {
+                int front = q.front();
+                q.pop();
+                
+                if(front == n-1)
+                {
+                    return steps;
+                }
+                
+                int left = front - 1;
+                int right = front + 1;
+                
+                if(right < n && !visited[right])
+                {
+                    q.push(right);
+                    visited[right] = 1;
+                }
+
+                if(left >= 0 && !visited[left])
+                {
+                    q.push(left);
+                    visited[left] = 1;
+                }  
+                
+                for(auto & x: mp[arr[front]])
+                {
+                     if(!visited[x]) 
+                     {
+                        q.push(x);
+                        visited[x] = 1;
+                    }
+                }
+                mp.erase(arr[front]);
+            }  
+            steps ++;
+            
+        }
+        return steps;
+    }
+};
