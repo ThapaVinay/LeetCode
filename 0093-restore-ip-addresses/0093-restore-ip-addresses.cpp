292042aa359205a4b20c80e7326b36beb7f31e09
Vinay Singh Thapa <97795121+ThapaVinay@users.noreply.github.com>
2024-02-05 16:32:07 +0530

    Time: 0 ms (100.00%), Space: 10.5 MB (23.25%) - LeetHub

---
+++ 0093-restore-ip-addresses/0093-restore-ip-addresses.cpp
@@ -0,0 +1,65 @@
+class Solution {
+public:
+    
+    void recur(string s, vector<string> &res, vector<string> &temp, int i)
+    {
+        if(i == s.size())
+        {
+            if(temp.size() == 4)
+            {
+                string s = "";
+                for(int j=0; j<4; j++)
+                {
+                    s += temp[j];
+                    if(j < 3)
+                        s += '.';
+                }
+                res.push_back(s);
+            }
+            return;
+        }
+        
+        if(temp.size() >= 4)
+            return;
+        
+        // we can either select one, two or three characters
+        
+        // one character
+        temp.push_back(s.substr(i, 1));
+        recur(s, res, temp, i+1);
+        temp.pop_back();
+        
+        // two characters
+        if(i+1 < s.size() && s[i] != '0')
+        {
+            temp.push_back(s.substr(i, 2));
+            recur(s, res, temp, i+2);
+            temp.pop_back();
+        }
+        
+        // three characters
+        if(i+2 < s.size() && s[i] != '0' && s[i] <= '2')
+        {
+            string temp1 = s.substr(i, 3);
+            
+            if(stoi(temp1) > 255)
+                return;
+            
+            temp.push_back(temp1);
+            recur(s, res, temp, i+3);
+            temp.pop_back();
+        }
+    }
+        
+    vector<string> restoreIpAddresses(string s) {
+        
+        int n = s.size();
+
+        vector<string> res;
+        vector<string> temp;
+        
+        recur(s, res, temp, 0);
+        
+        return res;
+    }
+};
